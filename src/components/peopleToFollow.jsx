d3795a762a19026e60d6f4ecfbfb722f3ef8fd7a
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-19 19:16:45 +0200

    rewrote people to follow in React

--- src/components/peopleToFollow.jsx
+++ src/components/peopleToFollow.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import { persons } from '../data'
 
 const PeopleToFollow = () => {
   return (
     <div className="gap-6 flex flex-col p-6 col-span-3 row-span-3 card-bg rounded-2xl">
           <h3 className="font-bold text-lg text-white">People you might want to follow</h3>
-          <template id="persontemplate">
-            <div className="flex gap-4 items-center">
-              <img className="avatar" height="40" width="40" alt="user portrait" />
+          {persons.map(({id, name, handle, avatar}) => (
+            <div key={id} className="flex gap-4 items-center">
+              <img src={avatar} height="40" width="40" alt="user portrait" />
               <div className="flex flex-col gap-1">
-                <p className="name text-sm text-white">Bernie Learney</p>
-                <p className="handle text-gray-400 text-sm">@bernielearney</p>
+                <p className="text-sm text-white">{name}</p>
+                <p className="text-gray-400 text-sm">{handle}</p>
               </div>
               <div className="flex justify-end flex-1">
                 <button className="followbutton bg-white rounded-lg py-1 px-2 hover:bg-slate-300 cursor-pointer">
@@ -17,9 +17,8 @@ const PeopleToFollow = () => {
                 </button>
               </div>
             </div>
-          </template>
-          <div id="followpersoncontainer" className="flex flex-col gap-6">
-          </div>
+            
+          ))}
           <button className="w-fit rounded-lg py-1 px-2 cursor-pointer">
             <p className="text-xs text-gray-200 hover:text-gray-300">Discover more people</p>
           </button>
---
+++ src/data.js
@@ -0,0 +1,7 @@
+
+// Array of persons
+export const persons = [
+        {id: 1, name: 'Berney Cranston', handle: '@berneycranston', avatar: 'https://api.dicebear.com/9.x/personas/svg?seed=Aneka'},
+        {id: 2, name: 'Jane Doe', handle: '@janedoe', avatar: 'https://api.dicebear.com/9.x/personas/svg?seed=Riley'},
+        {id: 3, name: 'Santa Claus', handle: '@santaclaus', avatar: 'https://api.dicebear.com/9.x/personas/svg?seed=Alexander'},
+];
