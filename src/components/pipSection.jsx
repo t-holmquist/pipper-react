c2be4d94483cf66b89c9291ccba9309222d64f38
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-19 19:42:53 +0200

    Rewrote pipsection in React

--- src/components/pipSection.jsx
+++ src/components/pipSection.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
+import { pips } from '../data'
 
 const PipSection = () => {
     return (
-        <section id="pipsection" className="grid gap-2 overflow-auto h-full col-span-6 row-span-5 rounded-2xl">
-            <ul id="pipcontainer" className="pipcontainer">
-                {/* <!-- JavaScript function generates pips here -->
-            <!-- The pipList div makes sure that there always is a html structure so that the show more button is rendered at the bottom --> */}
-                <div id="pipList" className="flex flex-col gap-4">
-                    <template id="piptemplate">
-                        <li className="group flex flex-col gap-2 border border-slate-400 pip-bg rounded-2xl p-4 min-h-50 transition-colors">
+        <section id='pipsection' className="grid gap-2 overflow-auto h-full col-span-6 row-span-5 rounded-2xl">
+            <ul className="pipcontainer">
+                <div className="flex flex-col gap-4">
+                    {pips.map(({ id, text, avatar, timestamp }) => (
+                        <li key={id} className="group flex flex-col gap-2 border border-slate-400 pip-bg rounded-2xl p-4 min-h-50 transition-colors">
                             <div className="flex justify-between items-center">
                                 <div className="flex gap-2 items-center group-hover:translate-x-1 transition-transform">
-                                    <img className="userimage w-10 h-10 rounded-full bg-green-950 group-hover:rotate-8 transition-transform" src="" alt="avatar" />
-                                    <p className="name font-bold"></p>
+                                    <img className="w-10 h-10 rounded-full bg-green-950 group-hover:rotate-8 transition-transform" src={avatar} alt="avatar" />
+                                    {/* Check if the dicebear url contains felix else show Riley as name */}
+                                    <p className="font-bold">{avatar.toLowerCase().includes('felix') ? 'Felix' : 'Riley'}</p>
                                 </div>
                                 <div className="flex gap-2 items-center">
-                                    <p className="date text-xs"></p>
-                                    <button id="deletebutton" className="delete h-fit p-1 border border-slate-500 cursor-pointer hover:bg-red-500 hover:text-white rounded-lg text-xs">Delete</button>
+                                    {/* Slice the month, day, hour and minute data */}
+                                    <p className="text-xs">{timestamp.slice(5, 16)}</p>
+                                    {/* Render delete button if the user is Felix */}
+                                    {avatar.toLowerCase().includes('felix') && (
+                                        <button className="delete h-fit p-1 border border-slate-500 cursor-pointer hover:bg-red-500 hover:text-white rounded-lg text-xs">Delete</button>
+                                    )}
                                 </div>
                             </div>
-                            <p className="piptext"></p>
+                            <p>{text}</p>
                         </li>
-                    </template>
+                    ))}
                 </div>
                 <div className="flex justify-center w-full py-3">
-                    <button id="showmorebutton" className="bg-black text-white w-fit rounded-lg py-1 px-2 hover:bg-slate-800 cursor-pointer">Show more</button>
+                    <button className="bg-black text-white w-fit rounded-lg py-1 px-2 hover:bg-slate-800 cursor-pointer">Show more</button>
                 </div>
             </ul>
         </section>
