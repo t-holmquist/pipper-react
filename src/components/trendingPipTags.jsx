b3ccbc7333850486f39f6ade1f48b7912e4dac41
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-19 19:19:33 +0200

    Rewrite done - trending pip tags to React

--- src/components/trendingPipTags.jsx
+++ src/components/trendingPipTags.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { pipTags } from '../data'
 
 const TrendingPipTags = () => {
   return (
@@ -6,13 +7,12 @@ const TrendingPipTags = () => {
           <h3 className="font-bold text-lg text-white">Trending pip tags</h3>
           <p className="text-gray-400">Trending in Austria</p>
           <div id="pipTagContainer" className="flex flex-col gap-4">
-            {/* <!-- Individal tag group queryselector gets them by classNamename and sets values --> */}
-            <template id="pipTagTemplate">
-              <div className="flex gap-6 items-center justify-between">
+            {pipTags.map(({id, title, numberOfPips}) => (
+              <div key={id} className="flex gap-6 items-center justify-between">
                 <p className="text-2xl text-white">#</p>
                 <div className="flex flex-col">
-                  <p className="title text-sm text-white"></p>
-                  <p className="numberOfPips text-gray-400 text-sm"></p>
+                  <p className="text-sm text-white">{title}</p>
+                  <p className="text-gray-400 text-sm">{numberOfPips}</p>
                 </div>
                 <div className="flex-1 flex justify-end">
                   <button className="rounded-full py-1 px-2 cursor-pointer">
@@ -20,7 +20,7 @@ const TrendingPipTags = () => {
                   </button>
                 </div>
               </div>
-            </template>
+            ))}
           </div>
     </div>
   )
--- src/data.js
+++ src/data.js
@@ -5,3 +5,14 @@ export const persons = [
         {id: 2, name: 'Jane Doe', handle: '@janedoe', avatar: 'https://api.dicebear.com/9.x/personas/svg?seed=Riley'},
         {id: 3, name: 'Santa Claus', handle: '@santaclaus', avatar: 'https://api.dicebear.com/9.x/personas/svg?seed=Alexander'},
 ];
+
+
+// Array of piptags
+export const pipTags = [
+        {id: 1, title: 'Home automation', numberOfPips: '87.2k'},
+        {id: 2, title: 'Ascii art', numberOfPips: '30.7k'},
+        {id: 3, title: 'Minimalism', numberOfPips: '20.1k'},
+        {id: 4, title: 'Sunglasses', numberOfPips: '5.3k'},
+        {id: 5, title: 'Running shoes', numberOfPips: '68.9k'},
+        {id: 6, title: 'UX design', numberOfPips: '34.3k'},
+];
