6a80d2fe2d4292968239903af54a1f2853fce3d9
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-24 21:26:16 +0200

    Update delete button on pips to cross and initial pip slice

--- src/components/pipSection.jsx
+++ src/components/pipSection.jsx
@@ -6,7 +6,7 @@ const PipSection = ({pips, setPips}) => {
 
 
     // Keeps track of the current slice end number to load/map over x amount of pips at a time
-    const [currentSliceNumber, setCurrentSliceNumber] = useState(3)
+    const [currentSliceNumber, setCurrentSliceNumber] = useState(4)
 
    
     const deletePip = (id) => {
@@ -65,11 +65,13 @@ const PipSection = ({pips, setPips}) => {
                                     </div>
                                     <div className="flex gap-2 items-center">
                                         {/* Slice the month, day, hour and minute data */}
-                                        <p className="text-xs">{timestamp}</p>
+                                        <p className="text-xs text-gray-500">{timestamp}</p>
                                         {/* Render delete button if the user is Felix */}
                                         {avatar.toLowerCase().includes('felix') && (
                                             // Calls delete pip function that takes an id to delete pip from mock database
-                                            <button onClick={() => deletePip(id)} className="delete h-fit p-1 border border-slate-500 cursor-pointer hover:bg-red-500 hover:text-white rounded-lg text-xs">Delete</button>
+                                            <button onClick={() => deletePip(id)} className="delete h-fit p-0.5 border border-slate-500 cursor-pointer hover:bg-red-500 hover:text-white rounded-lg text-xs">
+                                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                                            </button>
                                         )}
                                     </div>
                                 </div>
